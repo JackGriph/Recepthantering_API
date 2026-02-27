232120a2ec5a28193ed22b57731e67db4a4e0267
Joel Lindström <joel.tommy.lindstrom.syne25lin@edu.tucsweden.se>
2026-02-27 11:34:06 +0100

    created new DTO

---
+++ Recepthantering_API/Models/DTO/RecipeDTO.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Recepthantering_API.Models
+{
+    public class RecipeDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
