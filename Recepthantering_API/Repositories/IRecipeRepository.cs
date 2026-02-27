7ac46d34be6c3c4b0d980780fcaab7d29dc8752f
Sebastian <Sebastianbrindmark@gmail.com>
2026-02-27 10:50:56 +0100

    Created Repositories and services

--- Recepthantering_API/Repositories/IRecipeRepository.cs
+++ Recepthantering_API/Repositories/IRecipeRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace Recepthantering_API.Repositories
+﻿using Recepthantering_API.Models;
+
+namespace Recepthantering_API.Repositories
 {
     public interface IRecipeRepository
     {
+        Task<IEnumerable<Recipe>> GetAllAsync();
+        Task<Recipe?> GetByIdAsync(int id);
+        Task<IEnumerable<Recipe>> SearchAsync(string term);
+        Task<IEnumerable<Recipe>> GetByDifficultyAsync(string difficulty);
+        Task<Recipe> CreateAsync(Recipe recipe);
+        Task<Recipe?> UpdateAsync(int id, Recipe recipe);
+        Task<bool> DeleteAsync(int id);
     }
 }
